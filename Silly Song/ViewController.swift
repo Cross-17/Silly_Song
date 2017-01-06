ee5221e3412356e20437953cf9509947f3c5db53
政达 何 <Crossbell@zhengdadeMacBook-Pro.local>
2017-01-06 23:49:01 +0800

    second submission

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 func shortNameFromName(name: String) -> String {
-    // implementation here
     let result = name.lowercased()
+    
     let vowelSet = CharacterSet(charactersIn: "aeiou")
+    
     if let firstVowelRange = result.rangeOfCharacter(from: vowelSet) {
         return result.substring(from: firstVowelRange.lowerBound)
     }
@@ -24,6 +25,7 @@ let lyricsTemplate = "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>\n" +
     "Banana Fana Fo F<SHORT_NAME>\n" +
     "Me My Mo M<SHORT_NAME>\n" +
 "<FULL_NAME>"
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var lyricsView: UITextView!
@@ -32,15 +34,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         nameField.delegate = self
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     @IBAction func reset(_ sender: Any) {
         nameField.text = ""
         lyricsView.text = ""
     }
     @IBAction func displayLyrics(_ sender: Any) {
+         lyricsView.text = "heheda"
         lyricsView.text = lyricsForName(lyricsTemplate: lyricsTemplate, fullName: nameField.text!)
     }
 
